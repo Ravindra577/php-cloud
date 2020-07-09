4e35dcf0c9bad5c66a04bfed5d7a785659f468ea
Ravindra577 <ravindra.cse577@gmail.com>
2020-07-09 21:03:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <html>
 Hello Mario, Please Find Aws Ec2 Server Details: </br>
-<p> <?php echo exec('echo "Server Private IPAddress is <h2> $(hostname -i) </h2> 
-Server HostName is <h2> $(hostname) </h2>"'); ?> </p> </br>
+<p> <?php echo exec('echo "Server Private IPAddress is <h2> $(hostname -i) </h2> Server HostName is <h2> $(hostname) </h2>"'); ?> </p> </br>
 </html>
 
