972321513172ef439e912fd9753659b50542603d
Ravindra577 <ravindra.cse577@gmail.com>
2020-07-09 20:22:31 +0530

    added break and pharagraph

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <html>
-Hello Mario, Please Find Aws Ec2 Server Details:
-<?php echo exec('echo "Server Private IPAddress is $(hostname -i) and Server HostName is $(hostname)"'); ?>
+Hello Mario, Please Find Aws Ec2 Server Details: </br>
+<p> <?php echo exec('echo "Server Private IPAddress is $(hostname -i) and Server HostName is $(hostname)"'); ?> </p> </br>
 </html>
+
